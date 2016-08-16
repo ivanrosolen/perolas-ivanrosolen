35ab9e2c55f3f4881d9de2fd9034dc69ef01a3e4
Ivan Rosolen <ivan.rosolen@arizona.com.br>
2016-08-16 16:33:42 -0300

    dump

---
+++ db.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `perolas` (
+ `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+ `perola` varchar(2000) NOT NULL,
+ `nome` varchar(255) NOT NULL,
+ PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8
